1489a04d3d05cd27d7914efa6afb4401c317d7b9
Shirley Zhang <shirley.zhang@hotmail.com.au>
2017-04-03 07:54:52 +1000

    made incrementing dynamic

--- js.js
+++ js.js
@@ -3,24 +3,28 @@ function calc() {
 	var multi = document.getElementById("multi").value;
 	console.log(num +" * "+multi);
 	document.getElementById("ans").innerHTML = num*multi;
+	document.getElementById("working").innerHTML = "";
 }
 
 function calcN() {
 	var num = parseInt(document.getElementById("num").value);
-	var per = document.getElementById("twoSsPer").value;
+	var per = parseInt(document.getElementById("incrementPer").value);
+	var numIncrement = parseInt(document.getElementById("numIncrement").value);
 	var ans = 0;
 
 	var workspace = document.getElementById("working");
 	var calculations = "";
 
-	console.log(num+1);
 	for (var i = 1; i < num+1; i++) {
 		ans++;
 		calculations += "<div style='width:55px; float: left'>St "+i+": </div>"+ans
 
 		if (i % per == 0) {
-			ans++
-			calculations += " " + ans
+			// num increment - 1 because you added the first one above
+			for (var j = 0; j < (numIncrement-1); j++) {
+				ans++; 
+				calculations += " " + ans;	
+			}
 		}
 		calculations += "<br clear='both'>"
 
